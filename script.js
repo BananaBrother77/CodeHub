e5984cb3fa9794b421aad174acef426d556aefaa
BananaBrother77 <jonas.goltz2011@gmail.com>
2025-12-31 17:00:44 +0100

    update

--- script.js
+++ script.js
@@ -1,20 +1,67 @@
 function changeSection() {
-  const navBtns = document.querySelectorAll(".nav-btn");
-  const allSections = document.querySelectorAll(".tab");
-  navBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      const target = btn.getAttribute("data-target");
-      const activeSection = document.getElementById(target);
-
-      if (activeSection) {
-        allSections.forEach((section) => section.classList.add("disabled"));
-        activeSection.classList.remove("disabled");
+  const tabBtns = document.querySelectorAll('.card');
+  
+  tabBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+      const allTabs = document.querySelectorAll('.tab');
+      const targetId = btn.getAttribute('data-target');
+      const target = document.getElementById(targetId);
+      
+      if (target) {
+        allTabs.forEach(tab => tab.classList.remove('is-active'));
+        target.classList.add('is-active');
+        void target.offsetWidth;
         window.scrollTo(0, 0);
       }
+      
     });
   });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+const addCardBtn = document.getElementById('addCard');
+const addCodeMenu = document.getElementById('addCodeMenu');
+const addCodeOverlay = document.getElementById('overlay');
+const cancelBtn = document.getElementById('cancelBtn');
+const confirmBtn = document.getElementById('confirmBtn');
+
+const cardNameInput = document.getElementById('cardName');
+const cardDescInput = document.getElementById('cardDesc');
+const cardCodeInput = document.getElementById('cardCode');
+
+function showAddCodeMenu() {
+  addCardBtn.addEventListener('click', () => {
+    addCodeMenu.classList.add('is-visible');
+    addCodeOverlay.classList.add('is-visible');
+  });
+  
+  cancelBtn.addEventListener('click', () => cancelAddCodeMenu());
+  confirmBtn.addEventListener('click', () => confirmAddingCode());
+}
+
+function cancelAddCodeMenu() {
+   cardNameInput.value = '';
+   cardDescInput.value = '';
+   cardCodeInput.value = '';
+   
+   addCodeMenu.classList.remove('is-visible');
+   addCodeMenu.classList.add('is-closing');
+   addCodeOverlay.classList.remove('is-visible');
+   
+   setTimeout(() => {
+     addCodeMenu.classList.remove('is-closing');
+   }, 300);
+}
+
+function confirmAddingCode() {
+  if (cardNameInput.value.trim() !== '' && cardDescInput.value.trim() !== '' && cardCodeInput.value.trim() !== '') {
+    alert('success');
+  } else {
+    alert('error');
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
   changeSection();
+  showAddCodeMenu();
 });
